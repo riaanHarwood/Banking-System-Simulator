4cd81e613465e9b84f2239ed3543b6dfad550065
Riaan Harwood <104803762+riaanHarwood@users.noreply.github.com>
2025-08-04 19:57:41 +1200

    updated lines of code

--- backend/bank.py
+++ backend/bank.py
@@ -5,19 +5,24 @@ class Bank():
 
     def create_account(self, owner_name, type):
         return
+    
 
     def get_account(self, account_id):
         return
+     
     
     def deposit(self, account_id, amount): 
         return 
     
+    
     def withdraw(self, account_id, amount):
         return 
+    
     
     def transfer(self, from_id, to_id, amount):
         return 
     
+
     def get_transaction_history(self, account_id): 
         return 
     
